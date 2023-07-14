bbae2935a157599424329ea1efff461a92faa02e
janevnikola <72938546+janevnikola@users.noreply.github.com>
2023-07-14 22:01:37 +0200

    Create ProgramHelloWorld.cs

---
+++ C#-Tutorial-1/ProgramHelloWorld.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Tutorial_1{
+     class Program{
+       
+        static void Main(string[] args){
+            
+          
+            Console.WriteLine("Hello Nikola\n");//pecatime Hello na console
+            Console.WriteLine("Kako si deneska??");
+            Console.ReadLine();//dokolku ja nemavme ovaa linija kod, koga ke kliknevme start celo vreme ke se crashnuvase
+                               //ova Console.ReadLine funkcija ke go runnuva prozorceto t.e console se dodeka ne se pritisne nekoe kopce
+         
+        }
+    }
+}
